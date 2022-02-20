7d0d70e40c65bfb1122302701da2558314029b69
Abd Alwahid Mohammad Dahhan <s441008250@st.uqu.edu.sa>
2022-02-20 19:56:41 +0300

    excersize0

---
+++ main.js
@@ -0,0 +1,16 @@
+var msg = 'Good morning Welcome to Jacascript.'
+console.log(msg); //print message in msg
+
+var peg = require("pegjs");
+const fs = require("fs");
+
+grammerFile = 'grammer.pegjs'
+
+grammer = fs.readFileSync(grammarFile,'utf8')
+//console.log(grammar); //print grammar.pegjs code
+
+var parser = peg.generate(grammar)
+
+var input1 = '441008250'
+var output1 = parser.parse(input1)
+console.log(output1); //print amount of input1
